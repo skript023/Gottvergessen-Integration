855d99efb06cebf57b2ae11b96d48334d9f28f4a
unknown <fatur_muhammad@bk.ru>
2023-01-16 15:35:42 +0700

    add script local

--- database/migrations/2023_01_16_082438_create_script_locals_table.php
+++ database/migrations/2023_01_16_082438_create_script_locals_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSignaturesTable extends Migration
+class CreateScriptLocalsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateSignaturesTable extends Migration
      */
     public function up()
     {
-        Schema::create('signatures', function (Blueprint $table) {
+        Schema::create('script_locals', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('signature');
+            $table->integer('index');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ class CreateSignaturesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('signatures');
+        Schema::dropIfExists('script_locals');
     }
 }
